e3bf6c8d10e03b326d44b997025469f70833951b
Alejandra García <alejandra.garcialara@gmail.com>
2025-01-16 16:18:34 -0600

    Added support for delimiters of any length.

--- Program.fs
+++ Program.fs
@@ -6,7 +6,19 @@ let Add (input: string) =
       try 
           let processedInput = input.Replace("\\n", "\n")
           let numbers = 
-              if processedInput.StartsWith("//") then
+              if processedInput.StartsWith("//[") then 
+                  let endDelimiterIndex = processedInput.IndexOf("]")
+                  //extract delimiter (skip first 3 char)
+                  let delimiter = processedInput.Substring(3, endDelimiterIndex - 3)
+                  //get numbers part after ] and \n
+                  let numbersString = processedInput.Substring(endDelimiterIndex + 2)
+                  //split string by delimiter and convert to int
+                  numbersString.Split([|delimiter|], StringSplitOptions.RemoveEmptyEntries)
+                  |> Array.map int
+
+              // check if input starts with // 
+
+              else if processedInput.StartsWith("//") then
                   let delimiter = processedInput.[2] //get custom delimiter 
                   let numbersString = processedInput.[4..] // get the numbers part
                   numbersString.Split(delimiter) 
@@ -33,7 +45,7 @@ Console.WriteLine"Welcome to the String Calculator! What's your name?"
 
 let name = Console.ReadLine() // we get the user's name
 
-Console.WriteLine($"Hello {name}! Let's sum! \nType numbers separated by a comma. \n(You can use newline characters as well.)")
+Console.WriteLine($"Hello {name}! Let's sum! \nType numbers separated by a comma. \nYou can use newline characters as well. \nYou can also use custom delimiters of any length in this format: //[delimiter]new line character[numbers...].")
 let input = Console.ReadLine() // we get the numbers 
 
 let result = Add input
--- obj/Debug/net9.0/StringCalculatorF#.AssemblyInfo.fs
+++ obj/Debug/net9.0/StringCalculatorF#.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculatorF#")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762ef291858312b926c5a2edc32725f44ec50118")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71432989bdffcf8d97d8096ece8017a0730dd13d")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("StringCalculatorF#")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("StringCalculatorF#")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
